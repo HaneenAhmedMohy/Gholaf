fb1079c4060b7a802f9e49b9ef3d56edee448e31
Alaa Said <alaasaid1699@gmail.com>
2018-06-07 13:25:46 +0200

    cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  FservService } from '../fserv.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  cartData: Array<object>;
+  constructor( private data : FservService ) { 
+    this.cartData = [];
+    this.getCartData();
+  }
 
-  constructor() { }
+   //services
+   getCartData(): void {
+    let path: string = ' ./assets/cart.json';
+    this.data.getData(path).subscribe(
+      res => {
+        this.cartData = res;
+        // console.log(res);
+      },
+      err => {
+        console.log(err);
+      },
+      () => { }
+    );
+
+  }
 
   ngOnInit() {
   }
